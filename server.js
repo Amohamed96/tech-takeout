e773de85ed8bcb3bc57f7af80769cc083b48c64a
Connor Hughes <seanandhughes@gmail.com>
2021-11-28 13:37:04 -0500

    Adding login form, and styling

--- server.js
+++ server.js
@@ -52,6 +52,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
