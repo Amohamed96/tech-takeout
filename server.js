efb4aebe6fed14bc0cca03a092fac3f77cf2190a
Connor Hughes <seanandhughes@gmail.com>
2021-11-29 19:41:29 -0500

    adding table of orders, name, and form

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/checkout", (req, res) => {
+  res.render("checkout");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
